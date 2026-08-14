97af842206f340609f515b172ebdbd10bebbcf12
Fabrizio Rovelli <rovelli@gmail.com>
2026-08-14 10:01:01 +0200

    Harden optional I2C device detection and runtime status

--- include/env_service.h
+++ include/env_service.h
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_SHT31.h>
 #include <Adafruit_seesaw.h>
+
 #include "app_types.h"
 
 class EnvService {
@@ -14,12 +15,12 @@ public:
 
 private:
     bool readMoisture(uint16_t& value);
+    bool isI2cDevicePresent(uint8_t address);
 
     TwoWire* _wire = nullptr;
+
     Adafruit_SHT31 _sht31 = Adafruit_SHT31();
     Adafruit_seesaw _soil;
 
     EnvData _data;
-    bool readMoisture(uint16_t& value);
-    bool isI2cDevicePresent(uint8_t address);
 };
